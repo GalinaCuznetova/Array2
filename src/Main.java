31078c5c56f4935ba34b9befe2af1152119c815c
Galina_c <galina__k@mail.ru>
2022-09-23 00:21:17 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
+
 public class Main {
     public static void main(String[] args) {
+    }
+    int[] expenses = generateRandomArray();
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] expenses = new int[30];
+        for (int i = 0; i < expenses.length; i++) {
+            expenses[i] = random.nextInt(100_000) + 100_000;
+        }
+        return expenses;
+    }
+        System.out.println(Arrays.toString(expenses));
+
 
     }
-}
+
+
+    }
+
+
+
+
+
